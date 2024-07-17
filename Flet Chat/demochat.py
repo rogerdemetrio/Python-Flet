57ac11d725a106254e2386d9cdc370fd1b3eac9d
Roger Demetrio <rogerdemetrio@live.com>
2024-07-17 05:18:45 -0300

    Alterado varios atributos da pagina

--- Flet Chat/demochat.py
+++ Flet Chat/demochat.py
@@ -1,30 +1,38 @@
 import flet as ft
  
 def main(pagina):
+    # Atributos da pagina
+    pagina.title = "ChatChatChatLine"
+    pagina.bgcolor = ft.colors.GREY_700
+    pagina.padding = 0
+    pagina.spacing = 0
+    pagina.horizontal_alignment = "stretch"
+
+    # Variaveis globais
     texto = ft.Text("ChatChatChatLine")
 
-    chat = ft.Column()
+    chat = ft.Column(auto_scroll=True,scroll=ft.ScrollMode.HIDDEN)
 
     nome_usuario = ft.TextField(label="Nome do usuario")
 
+    #Tunel que envia a mensagem para os outros usarios
     def enviar_mensagem_tunel(mensagem):
         tipo = mensagem["tipo"]
         if tipo == "mensagem":
             texto_mensagem = mensagem["texto"]
             usuario_mensagem = mensagem["usuario"]
             # adicionar a mensagem no chat
-            chat.controls.append(ft.Text(f"{usuario_mensagem}: {texto_mensagem}"))
+            if mensagem["texto"] != "":
+                chat.controls.append(ft.Text(f"{usuario_mensagem}: {texto_mensagem}"))
         else:
             usuario_mensagem = mensagem["usuario"]
-            chat.controls.append(ft.Text(f"{usuario_mensagem} entrou no chat", 
-                                         size=12, italic=True, color=ft.colors.ORANGE_500))
+            chat.controls.append(ft.Text(f"{usuario_mensagem} entrou no chat", size=12, italic=True, color=ft.colors.YELLOW_ACCENT_700))
         pagina.update()
 
     pagina.pubsub.subscribe(enviar_mensagem_tunel)
 
     def enviar_mensagem(evento):
-        pagina.pubsub.send_all({"texto": campo_mensagem.value, "usuario": nome_usuario.value,
-                                "tipo": "mensagem"})
+        pagina.pubsub.send_all({"texto": campo_mensagem.value, "usuario": nome_usuario.value,"tipo": "mensagem"})
         # limpar o campo de mensagem
         campo_mensagem.value = ""
         pagina.update()
@@ -33,19 +41,19 @@ def main(pagina):
     botao_enviar_mensagem = ft.ElevatedButton("Enviar", on_click=enviar_mensagem)
 
     def entrar_popup(evento):
-        pagina.pubsub.send_all({"usuario": nome_usuario.value, "tipo": "entrada"})
-        # adicionar o chat
-        pagina.add(chat)
-        # fechar o popup
-        popup.open = False
-        # remover o botao iniciar chat
-        pagina.remove(botao_iniciar)
-        pagina.remove(texto)
-        # criar o campo de mensagem do usuario
-        # criar o botao de enviar mensagem do usuario
-        pagina.add(ft.Row(
-            [campo_mensagem, botao_enviar_mensagem]
-        ))
+        if nome_usuario.value != "":
+            pagina.pubsub.send_all({"usuario": nome_usuario.value, "tipo": "entrada"})
+            # adicionar o chat
+            # pagina.add(ft.Container(chat, padding=5, expand=True,bgcolor=ft.colors.GREY_500))
+            # fechar o popup
+            popup.open = False
+            # remover o botao iniciar chat e texto enviado
+            pagina.remove(botao_iniciar)
+            pagina.remove(texto)
+            # criar o campo de mensagem do usuario e o botao de enviar mensagem do usuario
+            pagina.add(ft.Container(chat, padding=25, expand=True),
+                       ft.Row([ft.Container(ft.Row([campo_mensagem, botao_enviar_mensagem]), padding=25, expand=True)]))
+        
         pagina.update()
 
     popup = ft.AlertDialog(
@@ -57,7 +65,7 @@ def main(pagina):
         )
 
     def entrar_chat(evento):
-        pagina.dialog = popup
+        pagina.overlay.append(popup)
         popup.open = True
         pagina.update()
 
@@ -66,6 +74,6 @@ def main(pagina):
     pagina.add(texto)
     pagina.add(botao_iniciar)
 
-ft.app(target=main, view=ft.WEB_BROWSER, port=8000)
+ft.app(target=main, view=ft.WEB_BROWSER, port=8080)
 
 # deploy
