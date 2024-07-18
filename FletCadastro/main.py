bbdc0dafd47b8594df19d245f37cbfcb968d7f76
Roger Demetrio <rogerdemetrio@live.com>
2024-07-18 06:23:31 -0300

    new file:   FletCadastro/__pycache__/modulos.cpython-312.pyc new file:   FletCadastro/__pycache__/produto.cpython-312.pyc modified:   FletCadastro/main.py new file:   FletCadastro/modulos.py

--- FletCadastro/main.py
+++ FletCadastro/main.py
@@ -1,39 +1,46 @@
 import flet as ft 
+import modulos
 
 titulo = ft.Text("Central de cadastros",size=24)
-labelProduto = ft.Text("Cadastro de produtos",size=12)
 
+#TODO: Criar modulos de cadastro:
+
+produto_tab = True
+pessoa_tab = True
+pesquisa_tab = True
 def main(p: ft.Page):
-    p.Title = "Cadastro de produtos"
+    p.Title = titulo
+
     p.navigation_bar = ft.CupertinoNavigationBar(
             bgcolor=ft.colors.BLACK45,
-            inactive_color=ft.colors.GREY,
-            active_color=ft.colors.GREEN_ACCENT_700,
-            on_change=lambda e: print("Selected tab:", e.control.selected_index),
+            inactive_color=ft.colors.WHITE70,
+            active_color=ft.colors.YELLOW_400,
+            icon_size=36,
+            on_change=lambda e: pag_index(e.control.selected_index),
             destinations=[
-                ft.NavigationBarDestination(icon=ft.icons.FASTFOOD_OUTLINED, label="Produtos"),
-                ft.NavigationBarDestination(icon=ft.icons.PEOPLE, label="Pessoas"),
-                ft.NavigationBarDestination(
-                    icon=ft.icons.BOOKMARK_BORDER,
-                    selected_icon=ft.icons.BOOKMARK,
-                    label="Explore",
-                ),
-            ]
-        )    
-    
-
-
+                ft.NavigationBarDestination(icon=ft.icons.FASTFOOD_OUTLINED,selected_icon=ft.icons.FASTFOOD, label="Produtos"),
+                ft.NavigationBarDestination(icon=ft.icons.PEOPLE_OUTLINED,selected_icon=ft.icons.PEOPLE, label="Pessoas"),
+                ft.NavigationBarDestination(icon=ft.icons.BOOK_OUTLINED,selected_icon=ft.icons.BOOK, label="Pesquisa"),
+            ],
+            
+        )
+    def pag_index(n):
+        print(n)
+        if n == 0:
+            modulos.cadastroProduto()
+        if n == 1:
+            modulos.cadastroPessoa()
+        if n == 2:
+            modulos.cadastroPesquisa()
+        else:
+            modulos.cadastroProduto()
+        
     p.update()
 ft.app(target=main)
 
-#TODO: Criar um arquivo principal que consiga encaixar outros modulos de cadastro e visualização de dados
-#TODO: Criar modulos de cadastro{
+#TODO: Criar uma tela principal que consiga encaixar outros modulos de cadastro e visualização de dados
+
+#TODO: Conectar todos os modulos ao main:
     # Modulo de produto
     # Modulo de pessoa
     # Modulo de pesquisa de mercado
-    #}
-#TODO: Conectar todos os modulos ao main{
-    # Modulo de produto
-    # Modulo de pessoa
-    # Modulo de pesquisa de mercado
-    #}
---
+++ FletCadastro/modulos.py
@@ -0,0 +1,16 @@
+import flet as ft 
+
+tituloPagina = ft.Text("Cadastro de produtos",size=12)
+
+#TODO:[prioridade] Modulo de produto
+def cadastroProduto():
+    print("Tela de produto")
+    
+    
+#TODO: Modulo de pessoa
+def cadastroPessoa():
+    print("Tela de pessoa")
+
+#TODO: Modulo de pesquisa de mercado
+def cadastroPesquisa():
+    print("Tela de pesquisa")
