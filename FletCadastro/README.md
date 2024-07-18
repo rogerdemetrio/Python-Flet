0fb6ddebb8a50966c3420e1c9c781cf0015e53ab
Roger Demetrio <rogerdemetrio@live.com>
2024-07-18 04:24:12 -0300

    New file:   FletCadastro/README.md Modified:   FletCadastro/main.py

---
+++ FletCadastro/README.md
@@ -0,0 +1,6 @@
+## Central de cadastros em FLET
+
+Hoje podemos cadastrar: WIP
+>- [ ] Produtos, com seus preços e detalhes
+>- [ ] Pessoas, com seus endereços e contatos
+>- [ ] Pesquisas de mercado customizadas
--- FletCadastro/main.py
+++ FletCadastro/main.py
@@ -1,2 +1,12 @@
 import flet as ft 
 
+def main(p):
+    pass
+
+
+
+ft.app(target=main)
+
+#TODO: Criar um arquivo principal que consiga encaixar outros modulos de cadastro e visualização de dados
+#TODO: Criar modulos de cadastro
+#TODO: Conectar todos os modulos ao main
