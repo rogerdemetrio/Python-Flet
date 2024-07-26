653b5482ffa17dd9548e6c49d53503a35f0ba38c
Roger Demetrio <rogerdemetrio@live.com>
2024-07-26 06:46:23 -0300

    Alterado comentários para explicar cada ponto feito

--- FletCadastro/classes.py
+++ FletCadastro/classes.py
@@ -2,7 +2,7 @@ import flet as ft
 
 # Classe padrão dos TextField da main
 class InputField(ft.TextField):
-    def __init__(self,label="",multiline=False,min_lines=1,max_lines=1):
+    def __init__(self,label="",hint_text="Digite aqui seu texto",multiline=False,min_lines=1,max_lines=1,input_filter=ft.InputFilter(allow=False, regex_string=r"[#$@/*=+(){}[!?;:&%]", replacement_string="")):
         super().__init__()
         self.label=label
         self.bgcolor=ft.colors.GREY_800
@@ -11,11 +11,19 @@ class InputField(ft.TextField):
         self.multiline = multiline
         self.min_lines = min_lines
         self.max_lines = max_lines
+        self.input_filter = input_filter
         #self.border=ft.InputBorder.NONE
         #self.filled=True
-        self.hint_text="Digite aqui"
+        self.hint_text=hint_text
 
 class LinhaDiv(ft.Divider):
     def __init__(self):
         super().__init__()
-        self.color = ft.colors.WHITE54
+        self.color = ft.colors.BLACK45
+
+def header():
+    return ft.Column([ft.Container(content=ft.Text(spans=[
+        ft.TextSpan("InfoDeme ",ft.TextStyle(color="#333333")), 
+        ft.TextSpan("Cadastros",ft.TextStyle(color="#111111"))],
+        theme_style=ft.TextThemeStyle.HEADLINE_LARGE),alignment=ft.alignment.center), 
+        LinhaDiv()])
--- FletCadastro/main.py
+++ FletCadastro/main.py
@@ -13,10 +13,10 @@ def main(page: ft.Page):
     page.window.resizable = False
     page.window.shadow = True
 
-    # Cores, titulo e alguns outros padrões basicos
+    # Cores, titulo e alguns outros parametros basicos
     page.title = "Central de cadastros"
-    page.theme = ft.Theme(color_scheme_seed="orange")
-    page.bgcolor = ft.colors.GREY_900
+    page.theme = ft.Theme(color_scheme_seed=ft.colors.RED_900)
+    page.bgcolor = ft.colors.GREY_500
     page.window.center()
     page.padding = 20
     
@@ -27,25 +27,29 @@ def main(page: ft.Page):
             icon_size=36,
             on_change=lambda e: pag_index(e.control.selected_index),
             destinations=[
-                ft.NavigationBarDestination(icon=ft.icons.FASTFOOD_OUTLINED,selected_icon=ft.icons.FASTFOOD, label="Produtos"),
-                ft.NavigationBarDestination(icon=ft.icons.PEOPLE_OUTLINED,selected_icon=ft.icons.PEOPLE, label="Pessoas"),
-                ft.NavigationBarDestination(icon=ft.icons.BOOK_OUTLINED,selected_icon=ft.icons.BOOK, label="Pesquisa"),
+                # Busca a lista de destinos que são os botões inferiores, 
+                # é criado dinamicamente conforme configurado no dictionary.py
+                l.lista[x]["dest"] for x in range(len(l.lista))
             ],
         )
     
     # Função que adiciona as informações no banco de dados 
     # quando o botão flutuante é acionado
     def btn_add(ev):
+        # Pega os valores dos inputs nas telas em forma de lista
         i = [inputText.value for inputText in body.controls]
+        # Cria uma tabela vazia
         tabela = {}
+        # Laço para formar um dicionario com os dados de coluna e input ficando assim {'coluna': 'input', 'coluna2': 'input2'}. 
+        # Esse dicionario vem da junção da dos valores da "col" que ficam no dicionary.py, 
+        # com os valores encontrados nos campos de input na tela principal
         for x in range(l.conta_lista(l.lista[pagina])):
             tabela.update({str(l.lista[pagina]["col"][x]):str(i[x])})
-        if pagina == 0:
-            tb = md.Produto(**tabela)
-        if pagina == 1:
-            tb = md.Pessoa(**tabela)
-        if pagina == 2:
-            tb = md.Pesquisa(**tabela)
+        # Depois de formado o dicionario, é criado essa variavel para "quebrar" a lista 
+        # passando na frente dela o parametro de cada tabela correspondente a tela. 
+        # Ficando assim: md.Produto({'coluna'= 'input', 'coluna2'= 'input2'}) 
+        # e depois passando para o banco através da session e comitando
+        tb = l.lista[pagina]["tb"](**tabela)
         md.session.add(tb)
         md.session.commit()
 
@@ -53,11 +57,10 @@ def main(page: ft.Page):
     # (fora botão e navegação)
     def pag_index(ev):
         global body,pagina
-        header = ft.Column([ft.Container(content=ft.Text("DemetrioVendas",theme_style=ft.TextThemeStyle.HEADLINE_LARGE),alignment=ft.alignment.center), cl.LinhaDiv()])
         pagina = ev
         page.controls.clear() 
-        body = ft.Column(controls=[l.lista[ev]["inputs"][x] for x in range(l.conta_lista(l.lista[ev]))])
-        page.add(ft.Column(controls=[header]),ft.Column(controls=[l.lista[ev]["tit"]]),
+        body = ft.Column(controls=[l.lista[pagina]["inputs"][x] for x in range(l.conta_lista(l.lista[pagina]))])
+        page.add(ft.Column(controls=[cl.header()]),ft.Column(controls=[l.lista[pagina]["tit"]]),
                  ft.Column(controls=[body]),ft.Column(controls=[cl.LinhaDiv()]))
 
     # Botão flutuante e posicionamento
