1211583334d14cbc15cb37f5269fa7aaed5a933c
Roger Demetrio <rogerdemetrio@live.com>
2024-07-29 04:39:24 -0300

    Ajustes na tabela que mostra os itens cadastrados no banco de dados

--- FletCadastro/main.py
+++ FletCadastro/main.py
@@ -62,7 +62,7 @@ def main(page: ft.Page):
         page.add(ft.Column(controls=[cl.header()]),ft.Column(controls=[l.lista[pagina]["tit"]]),
                  ft.Column(controls=[body]),ft.Column(controls=[cl.LinhaDiv()]))
         
-        # Caso no dicionario o botão esteja visivel = não, cai no if e não mostra o mesmo na tela.
+        # Caso no dicionario o botão esteja vis = False, cai no if e não mostra o mesmo na tela.
         if not l.lista[pagina]["vis"]:
             page.floating_action_button = ""
             page.update()
@@ -71,40 +71,53 @@ def main(page: ft.Page):
             page.floating_action_button = ft.FloatingActionButton(icon=ft.icons.ADD, on_click=btn_add,)
             page.floating_action_button_location = ft.FloatingActionButtonLocation.CENTER_FLOAT
             page.update()
+
             # Cria o corpo da tabela com as colunas
-            my_table = ft.DataTable(columns=[ft.DataColumn(ft.Text(str(l.lista[pagina]["col"][x][1]),color=ft.colors.BLACK87)) for x in range(l.conta_lista(l.lista[pagina]))],rows=[],)
+            my_table = ft.DataTable(columns=[ft.DataColumn(ft.Text(str(l.lista[pagina]["col"][x][1]),color=ft.colors.WHITE38)) for x in range(l.conta_lista(l.lista[pagina]))]
+                                    , rows=[], width = page.window.width, horizontal_lines = ft.BorderSide(1, ft.colors.GREY_600), horizontal_margin = 8, bgcolor = ft.colors.BLACK54,column_spacing=80)
             # Traz o nome da tabela a ser processada
             tab = l.lista[pagina]["table"]
+
             # busca a lista de colunas do dicionario
             colunas = [(l.lista[pagina]["col"][x][0]) for x in range(l.conta_lista(l.lista[pagina]))]
+
             # tranforma a lista em uma string com "," entre as colunas
             unpacked = ", ".join([e for e in colunas])
+
             # cria o select com a tabela e as colunas previamente processadas
             result = md.sql.text(f"SELECT {unpacked} from {tab}")
+
             # Conecta no banco pra trazer as informações ja cadastradas
             query = md.session.execute(result).fetchall()
+
             # Cria o dataRow que vai compor a tabela com as celulas vazias
             dtRow = ft.DataRow(cells=[])
+
             # busca todas as tuplas derivadas do select feito anteriormente e passa para a variavel em forma de lista
             for tupla in query:
                 # busca cada valor dentro da tupla do select feito anteriormente
                 for valueX in tupla._tuple():
                     # Cria a variavel da celula e coloca os valores dentro de cada celula individualmente
-                    dtCell = ft.DataCell(content=ft.Text(value=valueX ,color=ft.colors.BLACK))
+                    dtCell = ft.DataCell(content=ft.Text(value=valueX ))
                     dtRow.cells.append(dtCell)
+
                 # Coloca cada linha processada dentro da tabela
                 my_table.rows.append(dtRow)
+
                 # Limpa a linha processada anteriormente
                 dtRow = ft.DataRow(cells=[])
+
             # Adiciona um container expandido, depois a tabela criada 
             # e um outro conrtainer para gerar um espaço para o botão de adicionar os valores no banco
-            page.add(ft.Container(expand=1))
-            page.add(ft.Container(content= ft.Column([ft.Row([my_table], scroll= ft.ScrollMode.ALWAYS)], scroll= ft.ScrollMode.ALWAYS),height=200), )
-            page.add(ft.Container(height = 50))
+            page.add(ft.Container(expand=True)
+                     ,ft.Container(content= ft.Column([ft.Row([my_table], scroll= ft.ScrollMode.AUTO)], scroll= ft.ScrollMode.ADAPTIVE),height=200,alignment=ft.alignment.center)
+                    ,ft.Container(height=50))
+            
             
     # Carrega a primeira tela ao abrir o aplicativo
     pag_index(0)
     page.update()
     
+
 if __name__ == "__main__":
     ft.app(target=main)
